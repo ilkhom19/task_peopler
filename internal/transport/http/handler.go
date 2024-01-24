194c78ba0f749c73df0e2c85b8738c0fb74141b8
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2024-01-24 23:14:17 +0600

    fix

--- internal/transport/http/handler.go
+++ internal/transport/http/handler.go
@@ -37,13 +37,6 @@ func (h *Handler) InitRouter() *mux.Router {
 func (h *Handler) respond(w http.ResponseWriter, r *http.Request, statusCode int, data interface{}) {
 	w.WriteHeader(statusCode)
 	if data != nil {
-		if err := json.NewEncoder(w).Encode(data); err != nil {
-			h.error(w, r, http.StatusInternalServerError, err)
-			return
-		}
+		_ = json.NewEncoder(w).Encode(data)
 	}
-}
-
-func (h *Handler) error(w http.ResponseWriter, r *http.Request, serverError int, err error) {
-	h.respond(w, r, serverError, map[string]string{"error": err.Error()})
 }
