95745bd571a7fde022fe2186c484a2adfdd35d5d
ilkhom19 <ilkhomzhon.sidikov@gmail.com>
2024-01-25 09:46:00 +0600

    make saving robust

--- pkg/api_client.go
+++ pkg/api_client.go
@@ -3,6 +3,7 @@ package pkg
 import (
 	"encoding/json"
 	"fmt"
+	log "github.com/sirupsen/logrus"
 	"net/http"
 )
 
@@ -16,19 +17,22 @@ func (c *APIClient) FetchAge(name string) (int, error) {
 	url := fmt.Sprintf("https://api.agify.io/?name=%s", name)
 	resp, err := http.Get(url)
 	if err != nil {
-		return 0, err
+		log.Error("Failed to fetch age:", err)
+		return 0, nil
 	}
 	defer resp.Body.Close()
 
 	var data map[string]interface{}
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
-		return 0, err
+		log.Error("Failed to decode age:", err)
+		return 0, nil
 	}
 
 	age, ok := data["age"].(float64)
 	if !ok {
-		return 0, fmt.Errorf("age not found in API response")
+		log.Error("Failed to parse age:", err)
+		return 0, nil
 	}
 
 	return int(age), nil
@@ -36,21 +40,25 @@ func (c *APIClient) FetchAge(name string) (int, error) {
 
 func (c *APIClient) FetchGender(name string) (string, error) {
 	url := fmt.Sprintf("https://api.genderize.io/?name=%s", name)
+	unknown := "unknown"
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		log.Error("Failed to fetch the gender:", err)
+		return unknown, nil
 	}
 	defer resp.Body.Close()
 
 	var data map[string]interface{}
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
-		return "", err
+		log.Error("Failed to decode the gender:", err)
+		return unknown, nil
 	}
 
 	gender, ok := data["gender"].(string)
 	if !ok {
-		return "", fmt.Errorf("gender not found in API response")
+		log.Error("Failed to parse the gender:", err)
+		return unknown, nil
 	}
 
 	return gender, nil
@@ -58,21 +66,25 @@ func (c *APIClient) FetchGender(name string) (string, error) {
 
 func (c *APIClient) FetchNationality(name string) (string, error) {
 	url := fmt.Sprintf("https://api.nationalize.io/?name=%s", name)
+	unknown := "unknown"
 	resp, err := http.Get(url)
 	if err != nil {
-		return "", err
+		log.Error("Failed to fetch the nationality:", err)
+		return unknown, err
 	}
 	defer resp.Body.Close()
 
 	var data map[string]interface{}
 	err = json.NewDecoder(resp.Body).Decode(&data)
 	if err != nil {
-		return "", err
+		log.Error("Failed to decode the nationality:", err)
+		return unknown, err
 	}
 
 	countries, ok := data["country"].([]interface{})
 	if !ok || len(countries) == 0 {
-		return "", fmt.Errorf("nationality not found in API response")
+		log.Error("Failed to parse the nationality:", err)
+		return unknown, nil
 	}
 
 	nationality := countries[0].(map[string]interface{})["country_id"].(string)
